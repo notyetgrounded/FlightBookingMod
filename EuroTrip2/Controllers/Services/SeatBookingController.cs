b8d9a309fc8d08ea8619fbe31204526b26bb346c
notyetgrounded <notyetgroundedmail.com>
2022-10-25 17:26:09 +0530

    MybookingsView

---
+++ EuroTrip2/Controllers/Services/SeatBookingController.cs
@@ -0,0 +1,19 @@
+﻿using EuroTrip2.Contexts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EuroTrip2.Controllers.Services
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeatBookingController : ControllerBase
+    {
+        protected readonly FlightDBContext _context;
+
+        public SeatBookingController(FlightDBContext context)
+        {
+            _context=context;
+        }
+        public IActionResult<IEnumerable<bookin BookSeat()
+    }
+}
--- EuroTrip2/Models/Booking.cs
+++ EuroTrip2/Models/Booking.cs
@@ -10,17 +10,17 @@ namespace EuroTrip2.Models
         public int PassengerAge { get; set; }
         public DateTime DateTime { get; set; }=DateTime.Now;
         public int Status { get; set; }
-        public int? Trip_Id { get; set; }
+        public int Trip_Id { get; set; }
 
         [ForeignKey("Trip_Id")]
         public Trip? Trip { get; set; }
 
-        public int? User_Id { get; set; }
+        public int User_Id { get; set; }
 
         [ForeignKey("User_Id")]
         public User? User { get; set; }
 
-        public int? Seat_Id { get; set; }
+        public int Seat_Id { get; set; }
 
         [ForeignKey("Seat_Id")]
         public Seat? Seat { get; set; }
